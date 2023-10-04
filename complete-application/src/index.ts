06118e43b11c9e8752fd43b734e2621001a6785e
Alex FusionAuth <alex.patterson@fusionauth.io>
2023-10-04 15:06:15 -0400

    pr updates

--- complete-application/src/index.ts
+++ complete-application/src/index.ts
@@ -15,16 +15,16 @@ const app = express();
 const port = 8080; // default port to listen
 
 if (!process.env.clientId) {
-    console.error('Missing clientId from .env');
-    process.exit();
+  console.error('Missing clientId from .env');
+  process.exit();
 }
 if (!process.env.clientSecret) {
-    console.error('Missing clientSecret from .env');
-    process.exit();
+  console.error('Missing clientSecret from .env');
+  process.exit();
 }
 if (!process.env.fusionAuthURL) {
-    console.error('Missing clientSecret from .env');
-    process.exit();
+  console.error('Missing clientSecret from .env');
+  process.exit();
 }
 const clientId = process.env.clientId;
 const clientSecret = process.env.clientSecret;
@@ -32,30 +32,30 @@ const fusionAuthURL = process.env.fusionAuthURL;
 
 // Validate the token signature, make sure it wasn't expired
 const validateUser = async (userTokenCookie: { access_token: string }) => {
-    // Make sure the user is authenticated.
-    if (!userTokenCookie || !userTokenCookie?.access_token) {
-        return false;
-    }
-    try {
-        let decodedFromJwt;
-        await verify(userTokenCookie.access_token, await getKey, undefined, (err, decoded) => {
-            decodedFromJwt = decoded;
-        });
-        return decodedFromJwt;
-    } catch (err) {
-        console.error(err);
-        return false;
-    }
+  // Make sure the user is authenticated.
+  if (!userTokenCookie || !userTokenCookie?.access_token) {
+    return false;
+  }
+  try {
+    let decodedFromJwt;
+    await verify(userTokenCookie.access_token, await getKey, undefined, (err, decoded) => {
+      decodedFromJwt = decoded;
+    });
+    return decodedFromJwt;
+  } catch (err) {
+    console.error(err);
+    return false;
+  }
 }
 
 
 const getKey: GetPublicKeyOrSecret = async (header, callback) => {
-    const jwks = jwksClient({
-        jwksUri: `${fusionAuthURL}/.well-known/jwks.json`
-    });
-    const key = await jwks.getSigningKey(header.kid) as RsaSigningKey;
-    var signingKey = key?.getPublicKey() || key?.rsaPublicKey;
-    callback(null, signingKey);
+  const jwks = jwksClient({
+    jwksUri: `${fusionAuthURL}/.well-known/jwks.json`
+  });
+  const key = await jwks.getSigningKey(header.kid) as RsaSigningKey;
+  var signingKey = key?.getPublicKey() || key?.rsaPublicKey;
+  callback(null, signingKey);
 }
 
 //Cookies
@@ -73,166 +73,166 @@ app.use(express.urlencoded());
 
 // Static Files
 //tag::static[]
-app.use('/static', express.static(path.join(__dirname, '../static/')))
+app.use('/static', express.static(path.join(__dirname, '../static/')));
 //end::static[]
 
 //tag::homepage[]
 app.get("/", async (req, res) => {
-    const userTokenCookie = req.cookies[userToken];
-    if (await validateUser(userTokenCookie)) {
-        res.redirect(302, '/account');
-    } else {
-        const stateValue = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
-        const pkcePair = await pkceChallenge();
-        res.cookie(userSession, { stateValue, verifier: pkcePair.code_verifier, challenge: pkcePair.code_challenge }, { httpOnly: true });
+  const userTokenCookie = req.cookies[userToken];
+  if (await validateUser(userTokenCookie)) {
+    res.redirect(302, '/account');
+  } else {
+    const stateValue = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+    const pkcePair = await pkceChallenge();
+    res.cookie(userSession, { stateValue, verifier: pkcePair.code_verifier, challenge: pkcePair.code_challenge }, { httpOnly: true });
 
-        res.sendFile(path.join(__dirname, '../templates/home.html'));
-    }
+    res.sendFile(path.join(__dirname, '../templates/home.html'));
+  }
 });
 //end::homepage[]
 
 //tag::login[]
 app.get('/login', (req, res, next) => {
-    const userSessionCookie = req.cookies[userSession];
+  const userSessionCookie = req.cookies[userSession];
 
-    // Cookie was cleared, just send back (hacky way)
-    if (!userSessionCookie?.stateValue || !userSessionCookie?.challenge) {
-        res.redirect(302, '/');
-    }
+  // Cookie was cleared, just send back (hacky way)
+  if (!userSessionCookie?.stateValue || !userSessionCookie?.challenge) {
+    res.redirect(302, '/');
+  }
 
-    res.redirect(302, `${fusionAuthURL}/oauth2/authorize?client_id=${clientId}&response_type=code&redirect_uri=http://localhost:${port}/oauth-redirect&state=${userSessionCookie?.stateValue}&code_challenge=${userSessionCookie?.challenge}&code_challenge_method=S256`)
+  res.redirect(302, `${fusionAuthURL}/oauth2/authorize?client_id=${clientId}&response_type=code&redirect_uri=http://localhost:${port}/oauth-redirect&state=${userSessionCookie?.stateValue}&code_challenge=${userSessionCookie?.challenge}&code_challenge_method=S256`)
 });
 //end::login[]
 
 //tag::oauth-redirect[]
 app.get('/oauth-redirect', async (req, res, next) => {
-    // Capture query params
-    const stateFromFusionAuth = `${req.query?.state}`;
-    const authCode = `${req.query?.code}`;
+  // Capture query params
+  const stateFromFusionAuth = `${req.query?.state}`;
+  const authCode = `${req.query?.code}`;
 
-    // Validate cookie state matches FusionAuth's returned state
-    const userSessionCookie = req.cookies[userSession];
-    if (stateFromFusionAuth !== userSessionCookie?.stateValue) {
-        console.log("State doesn't match. uh-oh.");
-        console.log("Saw: " + stateFromFusionAuth + ", but expected: " + userSessionCookie?.stateValue);
-        res.redirect(302, '/');
-        return;
+  // Validate cookie state matches FusionAuth's returned state
+  const userSessionCookie = req.cookies[userSession];
+  if (stateFromFusionAuth !== userSessionCookie?.stateValue) {
+    console.log("State doesn't match. uh-oh.");
+    console.log("Saw: " + stateFromFusionAuth + ", but expected: " + userSessionCookie?.stateValue);
+    res.redirect(302, '/');
+    return;
+  }
+
+  try {
+    // Exchange Auth Code and Verifier for Access Token
+    const accessToken = (await client.exchangeOAuthCodeForAccessTokenUsingPKCE(authCode,
+      clientId,
+      clientSecret,
+      `http://localhost:${port}/oauth-redirect`,
+      userSessionCookie.verifier)).response;
+
+    if (!accessToken.access_token) {
+      console.error('Failed to get Access Token')
+      return;
     }
+    res.cookie(userToken, accessToken, { httpOnly: true })
 
-    try {
-        // Exchange Auth Code and Verifier for Access Token
-        const accessToken = (await client.exchangeOAuthCodeForAccessTokenUsingPKCE(authCode,
-            clientId,
-            clientSecret,
-            `http://localhost:${port}/oauth-redirect`,
-            userSessionCookie.verifier)).response;
-
-        if (!accessToken.access_token) {
-            console.error('Failed to get Access Token')
-            return;
-        }
-        res.cookie(userToken, accessToken, { httpOnly: true })
-
-        // Exchange Access Token for User
-        const userResponse = (await client.retrieveUserUsingJWT(accessToken.access_token)).response;
-        if (!userResponse?.user) {
-            console.error('Failed to get User from access token, redirecting home.');
-            res.redirect(302, '/');
-        }
-        res.cookie(userDetails, userResponse.user);
-
-        res.redirect(302, '/account');
-    } catch (err: any) {
-        console.error(err);
-        res.status(err?.statusCode || 500).json(JSON.stringify({
-            error: err
-        }))
+    // Exchange Access Token for User
+    const userResponse = (await client.retrieveUserUsingJWT(accessToken.access_token)).response;
+    if (!userResponse?.user) {
+      console.error('Failed to get User from access token, redirecting home.');
+      res.redirect(302, '/');
     }
+    res.cookie(userDetails, userResponse.user);
+
+    res.redirect(302, '/account');
+  } catch (err: any) {
+    console.error(err);
+    res.status(err?.statusCode || 500).json(JSON.stringify({
+      error: err
+    }))
+  }
 });
 //end::oauth-redirect[]
 
 //tag::account[]
 app.get("/account", async (req, res) => {
-    const userTokenCookie = req.cookies[userToken];
-    if (!await validateUser(userTokenCookie)) {
-        res.redirect(302, '/');
-    } else {
-        res.sendFile(path.join(__dirname, '../templates/account.html'));
-    }
+  const userTokenCookie = req.cookies[userToken];
+  if (!await validateUser(userTokenCookie)) {
+    res.redirect(302, '/');
+  } else {
+    res.sendFile(path.join(__dirname, '../templates/account.html'));
+  }
 });
 //end::account[]
 
 //tag::make-change[]
 app.get("/make-change", async (req, res) => {
-    const userTokenCookie = req.cookies[userToken];
-    if (!await validateUser(userTokenCookie)) {
-        res.redirect(302, '/');
-    } else {
-        res.sendFile(path.join(__dirname, '../templates/make-change.html'));
-    }
+  const userTokenCookie = req.cookies[userToken];
+  if (!await validateUser(userTokenCookie)) {
+    res.redirect(302, '/');
+  } else {
+    res.sendFile(path.join(__dirname, '../templates/make-change.html'));
+  }
 });
 
 app.post("/make-change", async (req, res) => {
-    const userTokenCookie = req.cookies[userToken];
-    if (!await validateUser(userTokenCookie)) {
-        res.status(403).json(JSON.stringify({
-            error: 'Unauthorized'
-        }))
-        return;
-    }
-
-    let error;
-    let message;
-
-    var coins = {
-        quarters: 0.25,
-        dimes: 0.1,
-        nickels: 0.05,
-        pennies: 0.01,
-    };
-
-    try {
-        message = 'We can make change for';
-        let remainingAmount = +req.body.amount;
-        for (const [name, nominal] of Object.entries(coins)) {
-            let count = Math.floor(remainingAmount / nominal);
-            remainingAmount =
-                Math.round((remainingAmount - count * nominal) * 100) / 100;
-
-            message = `${message} ${count} ${name}`;
-        }
-        `${message}!`;
-    } catch (ex: any) {
-        error = `There was a problem converting the amount submitted. ${ex.message}`;
-    }
-    res.json(JSON.stringify({
-        error,
-        message
+  const userTokenCookie = req.cookies[userToken];
+  if (!await validateUser(userTokenCookie)) {
+    res.status(403).json(JSON.stringify({
+      error: 'Unauthorized'
     }))
+    return;
+  }
+
+  let error;
+  let message;
+
+  var coins = {
+    quarters: 0.25,
+    dimes: 0.1,
+    nickels: 0.05,
+    pennies: 0.01,
+  };
+
+  try {
+    message = 'We can make change for';
+    let remainingAmount = +req.body.amount;
+    for (const [name, nominal] of Object.entries(coins)) {
+      let count = Math.floor(remainingAmount / nominal);
+      remainingAmount =
+        Math.round((remainingAmount - count * nominal) * 100) / 100;
+
+      message = `${message} ${count} ${name}`;
+    }
+    `${message}!`;
+  } catch (ex: any) {
+    error = `There was a problem converting the amount submitted. ${ex.message}`;
+  }
+  res.json(JSON.stringify({
+    error,
+    message
+  }))
 
 });
 //end::make-change[]
 
 //tag::logout[]
 app.get('/logout', (req, res, next) => {
-    res.redirect(302, `${fusionAuthURL}/oauth2/logout?client_id=${clientId}`);
+  res.redirect(302, `${fusionAuthURL}/oauth2/logout?client_id=${clientId}`);
 });
 //end::logout[]
 
 //tag::oauth-logout[]
 app.get('/oauth2/logout', (req, res, next) => {
-    console.log('Logging out...')
-    res.clearCookie(userSession);
-    res.clearCookie(userToken);
-    res.clearCookie(userDetails);
+  console.log('Logging out...')
+  res.clearCookie(userSession);
+  res.clearCookie(userToken);
+  res.clearCookie(userDetails);
 
-    res.redirect(302, '/')
+  res.redirect(302, '/')
 });
 //end::oauth-logout[]
 
 // start the Express server
 //tag::app[]
 app.listen(port, () => {
-    console.log(`server started at http://localhost:${port}`);
+  console.log(`server started at http://localhost:${port}`);
 });
 //end::app[]
