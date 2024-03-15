99c4ee093870d71c30ab4c42dcd3f406e34901a1
Dan Moore <dan@fusionauth.io>
2024-03-15 11:58:00 -0600

    no need to use these other browsers right now, just install chromium

--- complete-application/playwright.config.js
+++ complete-application/playwright.config.js
@@ -37,17 +37,6 @@ module.exports = defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
-
     /* Test against mobile viewports. */
     // {
     //   name: 'Mobile Chrome',
