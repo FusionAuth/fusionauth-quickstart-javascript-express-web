cd8bf3e5e84d49867e4e00e127268b3cd1d87ef3
Alex Patterson <alex@codingcat.dev>
2025-07-27 21:56:59 -0400

    add vercel node and fix the export

--- complete-application/api/[...all].ts
+++ complete-application/api/[...all].ts
@@ -1,2 +1,3 @@
+import { VercelRequest, VercelResponse } from '@vercel/node';
 import app from "../src/index";
-export default app;
+export default (req: VercelRequest, res: VercelResponse) => app(req, res);
